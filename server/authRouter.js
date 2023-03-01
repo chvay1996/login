b59b9088fdafbf2f470150fc8326578b9a472099
chvay1996 <chvay1996@gmail.com>
2023-03-01 10:58:21 +0200

    debug error

--- server/authRouter.js
+++ server/authRouter.js
@@ -1,15 +1,22 @@
 const Router = require("express");
 const router = new Router();
 const controller = require("./authController");
-const { body } = require("express-validator");
+const { check } = require("express-validator");
+const authMiddleware = require("./middlewaree/authMiddleware");
+const roleMiddleware = require("./middlewaree/roleMiddleware");
 
-router.post("/registration", controller.registration, [
-  body("username", "Имя пользователя не может быть пустым").notEmpty(),
-  body("password", "Пароль должен быть больше 4 и меньше 15 символов").isLength(
-    { min: 4, max: 15 }
-  ),
-]);
+router.post(
+  "/registration",
+  [
+    check("username", "Имя пользователя не может быть пустым").notEmpty(),
+    check(
+      "password",
+      "Пароль должен быть больше 4 и меньше 15 символов"
+    ).isLength({ min: 4, max: 15 }),
+  ],
+  controller.registration
+);
 router.post("/login", controller.login);
-router.get("/users", controller.getUsers);
+router.get("/users", roleMiddleware(["ADMIN"]), controller.getUsers);
 
 module.exports = router;
